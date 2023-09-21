c2a0ba6ad3a52a4964082b51043ba98b03070ca1
zahaibalig <zahaibalig@gmail.com>
2023-09-22 01:59:24 +0200

    fixed built error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
